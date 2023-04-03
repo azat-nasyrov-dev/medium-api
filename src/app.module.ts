d8cd2577b332ee0cc51216653ef51f2715dc4be4
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-04-04 02:57:19 +0600

    created a user module with its own service, controller, entity and dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TagModule } from '@app/tag/tag.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormconfig from '@app/ormconfig';
+import { UserModule } from '@app/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
 })
 export class AppModule {}
