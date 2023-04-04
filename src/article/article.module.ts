48f9183a6800e8c185d67887eb6358ce6d60b99d
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-04-05 05:05:10 +0600

    completed the implementation of the article creation method, added relationships between entities

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticleEntity } from '@app/article/article.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ArticleEntity])],
   providers: [ArticleService],
   controllers: [ArticleController],
 })
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from '@app/user/user.entity';
+import { CreateArticleDto } from '@app/article/dto/createArticle.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ArticleEntity } from '@app/article/article.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ArticleService {
-  async createArticle() {
-    return 'create article in service';
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>,
+  ) {}
+
+  async createArticle(
+    currentUser: UserEntity,
+    createArticleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    const article = new ArticleEntity();
+    Object.assign(article, createArticleDto);
+
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+
+    article.slug = 'foo'; /* FOR TEST !!! */
+
+    article.author = currentUser;
+
+    return await this.articleRepository.save(article);
   }
 }
