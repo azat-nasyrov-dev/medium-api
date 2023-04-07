be78fe797105fad6ed7f83d59b16dce66affb720
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-04-07 08:01:55 +0600

    implemented user follow

--- src/profile/profile.constants.ts
+++ src/profile/profile.constants.ts
@@ -1,1 +1,2 @@
 export const PROFILE_NOT_EXIST_ERROR = 'Profile does not exist';
+export const PROFILE_BAD_REQUEST_ERROR = 'Follower and following cant be equal';
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -4,13 +4,19 @@ import { ProfileResponseInterface } from '@app/profile/types/profileResponse.int
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '@app/user/user.entity';
 import { Repository } from 'typeorm';
-import { PROFILE_NOT_EXIST_ERROR } from '@app/profile/profile.constants';
+import {
+  PROFILE_BAD_REQUEST_ERROR,
+  PROFILE_NOT_EXIST_ERROR,
+} from '@app/profile/profile.constants';
+import { FollowEntity } from '@app/profile/follow.entity';
 
 @Injectable()
 export class ProfileService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
   ) {}
 
   async getProfile(
@@ -27,6 +33,43 @@ export class ProfileService {
 
     return { ...user, following: false };
   }
+
+  async followProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new HttpException(PROFILE_NOT_EXIST_ERROR, HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        PROFILE_BAD_REQUEST_ERROR,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: {
+        followerId: currentUserId,
+        followingId: user.id,
+      },
+    });
+
+    if (!follow) {
+      const followToCreate = new FollowEntity();
+      followToCreate.followerId = currentUserId;
+      followToCreate.followingId = user.id;
+      await this.followRepository.save(followToCreate);
+    }
+
+    return { ...user, following: true };
+  }
+
   buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
     delete profile.email;
     return { profile };
